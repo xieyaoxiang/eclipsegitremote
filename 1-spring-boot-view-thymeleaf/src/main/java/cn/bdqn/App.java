e98e63b456527477ca9afdd8d8ca49094f8353d9
xyx <2423618200@qq.com>
2019-04-09 13:58:32 +0800

    发布项目spring-boot

---
+++ 1-spring-boot-view-thymeleaf/src/main/java/cn/bdqn/App.java
@@ -0,0 +1,18 @@
+package cn.bdqn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * Thymeleaf入门案例
+ *
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
---
+++ 1-spring-boot-view-thymeleaf/src/main/java/cn/bdqn/pojo/User.java
@@ -0,0 +1,63 @@
+package cn.bdqn.pojo;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String password;
+    private String telephone;
+    private String username;
+    private String isadmin;
+    
+    public User(Integer id,String name,Integer age) {
+    	this.id = id;
+    	this.name = name;
+    	this.age = age;
+    }
+    
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getIsadmin() {
+		return isadmin;
+	}
+	public void setIsadmin(String isadmin) {
+		this.isadmin = isadmin;
+	}
+    
+    
+}
