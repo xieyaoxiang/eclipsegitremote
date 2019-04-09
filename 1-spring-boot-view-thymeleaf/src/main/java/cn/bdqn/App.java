80c4cd2c0087b2a77c39faad900bdc9235d19d22
xieyaoxiang <49423382+xieyaoxiang@users.noreply.github.com>
2019-04-09 14:27:31 +0800

    Update App.java

--- 1-spring-boot-view-thymeleaf/src/main/java/cn/bdqn/App.java
+++ 1-spring-boot-view-thymeleaf/src/main/java/cn/bdqn/App.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.print("abc");
+		System.out.print("abc123");
 		SpringApplication.run(App.class, args);
 	}
 
